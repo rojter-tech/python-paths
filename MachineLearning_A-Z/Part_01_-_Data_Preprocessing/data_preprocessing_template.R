1c19dc9fcff046b0e4e4bb4bfd8eab08c29c2946
Daniel Reuter <dreuter@kth.se>
2019-09-02 15:30:11 +0200

    chg

--- MachineLearning_A-Z/Part_01_-_Data_Preprocessing/data_preprocessing_template.R
+++ MachineLearning_A-Z/Part_01_-_Data_Preprocessing/data_preprocessing_template.R
@@ -7,7 +7,7 @@ dataset = read.csv('Data.csv')
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$DependentVariable, SplitRatio = .8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
